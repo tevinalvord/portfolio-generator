e8f83fe78447be17cc844ed63938918c26cca178
tevinalvord <tevin.alvord@gmail.com>
2022-04-28 14:39:25 -0600

    can use node CL with parameters to create html file dynamically

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const profileDataArgs = process.argv.slice(2);
+const fs = require("fs");
 
-const printProfileData = (profileDataArr) => {
-    // This
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+const generatePage = require('./src/page-template.js');
 
-    console.log("===============");
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-    // Is the same as this
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
